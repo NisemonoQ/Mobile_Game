e5ea1482f1c38d61ea2ae541dcbce3d5a7096135
NisemonoQ <yann.badinga@gmail.com>
2023-11-21 10:53:49 +0100

    Auto stash before merge of "main" and "origin/main"

--- MobileGame/Assets/Scripts/Chara.cs
+++ MobileGame/Assets/Scripts/Chara.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Chara : MonoBehaviour
 {
     Rigidbody2D rb;
+    Transform cha; 
     private Vector2 toTheTop;
     public float moving = 10f;
 
@@ -18,15 +19,21 @@ public class Chara : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cha = GetComponent<Transform>();
     }
 
     private void Update()
     {
-        
+        /*toTheTop.x = moving;
+        toTheTop.y = cha.position.y;
+
+        Vector2 targetPosition = transform.position.x * transform.right;
+        transform.position = Vector2.Lerp(transform.position, targetPosition, moving * Time.deltaTime);*/
     }
 
     private void FixedUpdate()
     {
+        //rb.MovePosition(toTheTop * Time.deltaTime);
         Vector2 move = Vector2.right * Input.GetAxisRaw("Horizontal") * moving;
         move.y = rb.velocity.y;
 
