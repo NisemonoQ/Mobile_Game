d3869c16833cb51e648f44281c18d506bbeb6093
NisemonoQ <yann.badinga@gmail.com>
2023-11-21 11:22:34 +0100

    charactercontroller refait
    il n'est pas bon pour l'instant

--- MobileGame/Assets/Scripts/Chara.cs
+++ MobileGame/Assets/Scripts/Chara.cs
@@ -1,54 +1,74 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Chara : MonoBehaviour
 {
-    Rigidbody2D rb;
-    Transform cha; 
-    private Vector2 toTheTop;
-    public float moving = 10f;
-
-    bool jumpregister; 
-
-    bool up = false;
-    bool down = true;
-
-    bool grounded = true; 
+    private Rigidbody2D rb;
+    public float jumpHeight = 10.0f;
+    public float speed = 7.0f;
+    private bool grounded = true;
+    private bool jumpregister = false;
+    private GameObject ground;
+    public GameObject Enemy;
+    public float fall;
+    private Animator playerAnimator;
+    public float shortfall;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        cha = GetComponent<Transform>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        /*toTheTop.x = moving;
-        toTheTop.y = cha.position.y;
-
-        Vector2 targetPosition = transform.position.x * transform.right;
-        transform.position = Vector2.Lerp(transform.position, targetPosition, moving * Time.deltaTime);*/
+        if (grounded && Input.GetButtonDown("Jump"))
+        {
+            jumpregister = true;
+        }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        //rb.MovePosition(toTheTop * Time.deltaTime);
-        Vector2 move = Vector2.right * Input.GetAxisRaw("Horizontal") * moving;
+        Vector2 move = Vector2.right * Input.GetAxisRaw("Horizontal") * speed;
         move.y = rb.velocity.y;
-
-        if(jumpregister)
+        if (jumpregister)
         {
-            
+            move.y = CalculateJumpForce();
+            jumpregister = false;
+            playerAnimator.SetTrigger("Jump");
+            //grounded = false;
+        }
+        Debug.DrawRay(rb.position, move, Color.green);
+
+        if (move.y < -Mathf.Epsilon)
+        {
+            Debug.Log("Descente");
+            move.y += Physics2D.gravity.y * rb.gravityScale * fall * Time.deltaTime;
+        }
+        if (!grounded)
+        {
+            //Descente : 
+            if (move.y < 0f)
+            {
+                move.y += Physics2D.gravity.y * rb.gravityScale * fall * Time.deltaTime;
+            }
+            //On monte et la touche est relach�e : la gravit� est plus forte (on monte donc moins haut)
+            else if (!Input.GetButton("Jump"))
+            {
+                move.y += Physics2D.gravity.y * rb.gravityScale * shortfall * Time.deltaTime;
+            }
         }
 
         rb.velocity = move;
     }
 
-    void Gravitas()
+    private float CalculateJumpForce()
     {
-        
+
+        float jumpImpulse = Mathf.Sqrt(-2f * Physics2D.gravity.y * rb.gravityScale * jumpHeight);
+
+        return jumpImpulse;
     }
-
-
 }
