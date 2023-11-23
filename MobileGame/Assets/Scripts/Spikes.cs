0716f5965bd5307702f139770a9e37b0dd69ea96
NisemonoQ <yann.badinga@gmail.com>
2023-11-23 16:47:37 +0100

    load scene added

--- MobileGame/Assets/Scripts/Spikes.cs
+++ MobileGame/Assets/Scripts/Spikes.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Spikes : MonoBehaviour
 {
     public GameObject player;
+    float attend = 5f;
     //public MeshRenderer playerMesh;
 
     private void OnTriggerEnter(Collider other)
@@ -13,9 +16,24 @@ public class Spikes : MonoBehaviour
         {
             FMODUnity.RuntimeManager.PlayOneShot("event:/Mort");
             player.SetActive(false);
-            //playerMesh.enabled = false; 
-            
+            //playerMesh.enabled = false;             
             Debug.Log("He is dead");
+
+            
         }
     }
+
+    private void Update()
+    {
+        if(player.activeSelf == false)
+        {
+            attend -= 1.5f * Time.deltaTime; 
+        }
+
+        if(attend <= 0f)
+        {
+            SceneManager.LoadScene("Test LD 1", LoadSceneMode.Single);
+        }
+    }
+
 }
